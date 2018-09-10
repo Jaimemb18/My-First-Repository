6086ba5bdb26e02b7abdd454f21f366e0cec5a07
Jaimemb18 <jmb8534@rit.edu>
2018-09-09 20:23:36 -0400

    Initial Commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public Main(){
+        System.out.println("Hello World!");
+        System.out.println("Devin sign my packet...");
+    }
+
+    public static void main(String args[]){
+        new Main();
+    }
+}
